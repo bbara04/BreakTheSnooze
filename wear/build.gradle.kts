d8adf6b15998cb8c9fd7ba75256cca7ea337109e
bbara <bdbarni@gmail.com>
2025-11-06 13:27:05 +0100

    chore: update version code to 1000002 and set version name to 1

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "hu.bbara.breakthesnooze"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.1.1"
+        versionCode = 1000002
+        versionName = "1"
     }
 
     buildTypes {
